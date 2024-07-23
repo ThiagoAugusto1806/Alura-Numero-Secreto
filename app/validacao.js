32b6560cd1da84be10e9320631e360a17a162369
ThiagoAugusto1806 <thiagoaugusto1806@gmail.com>
2024-07-23 20:56:08 -0300

    .

--- app/validacao.js
+++ app/validacao.js
@@ -1,20 +1,8 @@
 function verificaSeChutePossuiValorValido(chute){
     const numero = +chute
     if(chuteForInvalido(numero)){
-        if (chute.toUpperCase() === "GAME OVER") {
-
-            document.body.innerHTML =
-                `
-                <h2>Game Over!!!</h2>
-                <h3>Pressione o botão para jogar novamente</h3>
-                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
-                `
-                document.body.style.backgroundColor = "black";
-        } else {
-
-            elementoChute.innerHTML += '<div>Valor Inválido</div>';
-            return
-        }
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        return
         
     }if(numeroForMaiorOuMenorQuePermitido(numero)){
         elementoChute.innerHTML += `<div>Valor Invalido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
